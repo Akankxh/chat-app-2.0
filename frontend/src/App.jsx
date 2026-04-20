3c7442c3d1d776279f54e0b1fb997325b3edd969
Akanksh <akankshmuvva23@gmail.com>
2026-04-20 17:51:52 +0530

    lol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import { useAuthStore } from './store/useAuthStore'
 import { useThemeStore } from './store/useThemeStore';
 import { use } from 'react'
 import { useEffect } from 'react'
-import Loader from 'lucide-react/dist/esm/icons/loader'
+import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
